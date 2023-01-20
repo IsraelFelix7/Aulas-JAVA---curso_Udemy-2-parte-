53ee677f4c54876f008b7979829b96446aa2473e
Israel Felix <israelfelixbenny@gmail.com>
2023-01-19 22:23:19 -0300

    nova atividade sobre enumerações

---
+++ enumeracoes_pedido/src/aplication/Program.java
@@ -0,0 +1,19 @@
+package aplication;
+
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
+public class Program {
+
+	public static void main(String[] args) {
+	System.out.println("Hello, world!");
+
+		Order order = new Order(0001, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+	
+	}
+
+}
