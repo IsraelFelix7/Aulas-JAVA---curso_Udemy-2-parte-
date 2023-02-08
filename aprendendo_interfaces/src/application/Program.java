acfe47c22620336b02b32ff71b6fa5ab1fc0c63c
Israel Felix <israelfelixbenny@gmail.com>
2023-02-08 20:27:56 -0300

    atualizações das aulas

--- aprendendo_interfaces/src/application/Program.java
+++ aprendendo_interfaces/src/application/Program.java
@@ -1,10 +1,34 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre com os dados do contrato: ");
+		System.out.println("Numero: ");
+		int numero = sc.nextInt();
+		System.out.println("Entre com a datav(dd/mm/yyyy): ");
+		LocalDate  date = LocalDate.parse(sc.next(), fmt);
+		System.out.println("Valor do contrato: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract contract = new Contract(numero, date, totalValue);
+		
+		System.out.println("Entre com o numero de parcelas: ");
+		
+		
+		sc.close();
 	}
 
 }
---
+++ aprendendo_interfaces/src/model/service/ContractService.java
@@ -0,0 +1,18 @@
+package model.service;
+
+import model.entities.Contract;
+
+public class ContractService {
+	
+	private OnlinePaymentService onlinePaymentService;
+
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+	
+	public void processContract(Contract contract, int morths) {
+		
+		
+	}
+	
+}
